98e4b4719e6d98ac2040804780b18e9085ffb5e8
ssul316 <ssul316@naver.com>
2014-10-28 17:52:22 +0900

    141028 responseBody 적용

--- src/main/java/org/han/vo/UserVO.java
+++ src/main/java/org/han/vo/UserVO.java
@@ -1,6 +1,6 @@
 package org.han.vo;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class UserVO {
 
--- src/main/java/org/han/web/BbsController.java
+++ src/main/java/org/han/web/BbsController.java
@@ -1,14 +1,18 @@
 package org.han.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.han.service.BbsService;
+import org.han.vo.BbsVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/bbs/*")
@@ -19,15 +23,18 @@ public class BbsController {
 	
 	private static Logger logger = LoggerFactory.getLogger(BbsController.class);
 	
+//	@RequestMapping("/list")
+//	public String list(@RequestParam(value="page", defaultValue="") String page, Model model){
+//		
+//		model.addAttribute("list" , service.list(page));
+//		
+//		return "bbs/list";
+//		
+//	}
+		
 	@RequestMapping("/list")
-	public String list(@RequestParam(value="page", defaultValue="") String page, Model model){
-		
-		model.addAttribute("list" , service.list(page));
-		
-		return "bbs/list";
-		
+	public @ResponseBody List<BbsVO> list(@RequestParam(value="page", defaultValue="1") String page, Model model){
+		return service.list(page);
 	}
-
- 
 
 }
